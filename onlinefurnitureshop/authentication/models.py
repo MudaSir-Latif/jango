6f76f527bbc3aeff06dc5564477d64cb4c35bfb2
Mudassir Latif <mudassirm.latif@gmail.com>
2024-06-29 19:01:14 +0500

    first commit

--- onlinefurnitureshop/authentication/models.py
+++ onlinefurnitureshop/authentication/models.py
@@ -3,14 +3,8 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    USER_TYPES = (
-        ('admin', 'Admin'),
-        ('regular', 'Regular'),
-        ('guest', 'Guest'),
-    )
     
     user_id = models.AutoField(primary_key=True)
-    # user_type = models.CharField(max_length=10, choices=USER_TYPES)
     password = models.CharField(max_length=128)
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
@@ -18,16 +12,16 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
-# class Address(models.Model):
-#     add_id = models.ForeignKey(User, related_name='addresses', on_delete=models.CASCADE)
-#     address = models.CharField(max_length=255)
+class Address(models.Model):
+    add_id = models.ForeignKey(User, related_name='addresses', on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
 
-#     def __str__(self):
-#         return self.address
+    def __str__(self):
+        return self.address
 
-# class PhoneNumber(models.Model):
-#     phone_id = models.ForeignKey(User, related_name='phone_numbers', on_delete=models.CASCADE)
-#     phone_number = models.CharField(max_length=20)
+class PhoneNumber(models.Model):
+    phone_id = models.ForeignKey(User, related_name='phone_numbers', on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
 
-#     def __str__(self):
-#         return self.phone_number
+    def __str__(self):
+        return self.phone_number
